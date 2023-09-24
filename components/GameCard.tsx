94a02ac0b0d9055738bcc69ab11b4a06e6a122dd
pa4080 <spas.z.spasov@metalevel.tech>
2023-09-24 21:51:20 +0300

    feat: add <GameCard /> component

---
+++ components/GameCard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Game } from "@/interfaces/game";
+import { cn } from "@/lib/cn-utils";
+
+interface Props {
+	game: Game;
+	className?: string;
+}
+
+const GameCard: React.FC<Props> = ({ game, className }) => {
+	return (
+		<div className={cn("", className)}>
+			<div>{game.name}</div>
+		</div>
+	);
+};
+
+export default GameCard;
--- components/GameGrid.tsx
+++ components/GameGrid.tsx
@@ -6,6 +6,8 @@ import useRawgGames from "@/hooks/useRawgGames";
 
 import { cn } from "@/lib/cn-utils";
 
+import GameCard from "./GameCard";
+
 interface Props {
 	className?: string;
 }
@@ -16,32 +18,30 @@ const GameGrid: React.FC<Props> = ({ className }) => {
 	return (
 		<div className={cn("", className)}>
 			<div className="flex gap-4 justify-center items-center mb-4 h-8">
-				{error ? (
-					<p className="text-lg text-red-500 font-semibold m-0">{error}</p>
-				) : (
-					<>
-						<button
-							className="btn_sm_affirmative"
-							disabled={!games?.previous}
-							onClick={() => games?.previous && getGamesBy(games?.previous)}
-						>
-							Prev
-						</button>
-						<button
-							className="btn_sm_affirmative"
-							disabled={!games?.next}
-							onClick={() => games?.next && getGamesBy(games?.next)}
-						>
-							Next
-						</button>
-					</>
-				)}
+				<button
+					className="btn_sm_affirmative"
+					disabled={!games?.previous}
+					onClick={() => games?.previous && getGamesBy(games?.previous)}
+				>
+					Prev
+				</button>
+
+				<button
+					className="btn_sm_affirmative"
+					disabled={!games?.next}
+					onClick={() => games?.next && getGamesBy(games?.next)}
+				>
+					Next
+				</button>
 			</div>
+
 			{isLoading || !games ? (
 				<div>Loading...</div>
 			) : (
-				<div>{games?.results.map((game, index) => <div key={index}>{game.name}</div>)}</div>
+				<div>{games?.results.map((game, index) => <GameCard key={index} game={game} />)}</div>
 			)}
+
+			{error && <p className="text-lg text-red-500 font-semibold m-0">{error}</p>}
 		</div>
 	);
 };
