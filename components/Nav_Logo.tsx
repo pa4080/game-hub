72470fd246eecd77378eca6f7b1fe9f75f24a128
pa4080 <spas.z.spasov@metalevel.tech>
2023-10-25 16:24:41 +0300

    fix: the logo component size

--- components/Nav_Logo.tsx
+++ components/Nav_Logo.tsx
@@ -15,17 +15,17 @@ const Nav_Logo: React.FC<Props> = ({ className }) => {
 			<div
 				className={cn("flex items-center justify-start gap-1 drop-shadow-xl text-3xl", className)}
 			>
-				<div className="inline-flex items-center justify-center rounded-md border-input h-11 w-11 flex-shrink-0">
+				<div className="inline-flex items-center justify-center rounded-md border-input h-[48px] w-[48px] flex-shrink-0">
 					<Image
 						priority
 						alt={messages.NavBar.logoDesc}
-						className="h-11 w-11 drop-shadow-xl"
-						height={44}
+						className="h-[48px] w-[48px] drop-shadow-xl"
+						height={48}
 						src={logo}
-						width={44}
+						width={48}
 					/>
 				</div>
-				<div className="font-unicephalon text-accent tracking-wider translate-y-[0.2rem] text-left line-clamp-1">
+				<div className="font-unicephalon text-accent tracking-wider translate-y-[3px] text-left line-clamp-1">
 					{messages.Home.title.substring(1)}
 				</div>
 			</div>
